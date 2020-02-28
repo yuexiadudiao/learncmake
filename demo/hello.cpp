31abd1ad212eab89a721524809ea827d1877aa57
yin <1206489318@qq.com>
2020-02-28 14:19:44 +0800

    create repo

---
+++ demo/hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<add.h>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	cout<<"please input two number:"<<endl;
+	cin>>a>>b;
+	cout<<a<<"+"<<b<<"="<<add(a,b)<<endl;
+	cout<<"this is cmake tutorial demo!"<<endl;
+}
